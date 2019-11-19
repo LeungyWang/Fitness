94a639977313f31afdab918e0f8072fb8c70a682
LengyWang <yuan_leung@icloud.com>
2019-11-20 04:53:20 +0800

    update 11/20 4:53

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import User
+from .models import New_User
 # Register your models here.
 
 
-admin.site.register(User)
+admin.site.register(New_User)
--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
+class New_User(models.Model):
 
 	vip_id = models.AutoField(primary_key=True)
 
