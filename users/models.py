9500cbec7ff288cd85e794f8ad2f6fb8468e5cb8
LengyWang <yuan_leung@icloud.com>
2019-11-20 00:03:16 +0800

    update users 11/20 0:03

--- users/models.py
+++ users/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+
+	vip_id = models.AutoField(primary_key=True)
+
+	vip_name = models.CharField(max_length=50)
+
+	vip_gender = (('male','男'),('female','女'))
+
+	vip_sex = models.CharField(max_length=10,choices=vip_gender)
+
+	vip_birthday = models.DateField()
+
+	vip_phone = models.CharField(max_length=30)
+
+	vip_email = models.EmailField(unique=True)
+
+	vip_duriation = models.CharField(max_length=30)
+
+	vip_level = models.IntegerField()
+
+	class Meta:
+		ordering = ('-vip_id',)
---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path('regist/',views.user_regist,name='user_regist'),
+]
