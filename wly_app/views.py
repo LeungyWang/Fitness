c0e8a4111969ab25798f8544784bcd2a647058d2
LengyWang <yuan_leung@icloud.com>
2019-12-03 16:03:16 +0800

    update wly_app and db

--- wly_app/views.py
+++ wly_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,redirect
 from .form import BodyCheckForm
+from .models import BodyData
+from . import models
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse,JsonResponse
-from . import models
 from django.contrib import messages
 # Create your views here.
 
@@ -10,14 +11,31 @@ def body_check(request):
     if request.method == "POST":
         form = BodyCheckForm(request.POST)
         if form.is_valid():
+            username = request.session.get("user_name")
+            if username is None:
+                raise Exception
             hight = form.cleaned_data["hight"]
-            hegith = form.cleaned_data["height"]
+            heigth = form.cleaned_data["height"]
             chest = form.cleaned_data["chest"]
             waist = form.cleaned_data["waist"]
             hip = form.cleaned_data["hip"]
             queith = form.cleaned_data["queith"]
             maxh = form.cleaned_data["maxh"]
-
+            if BodyData.objects.get(user_name=username):
+                bd = BodyData.objects.get(user_name=username)
+                bd.hight = hight
+                bd.height = heigth
+                bd.chest = chest
+                bd.waist = waist
+                bd.hip = hip
+                bd.queith = queith
+                bd.maxh = maxh
+                bd.save()
+            else:
+                bd = BodyData(user_name=username, hight=hight, height=heigth, chest=chest, waist=waist, hip=hip,
+                              queith=queith, maxh=maxh)
+                bd.save()
+            messages.success(request,"保存成功")
         else:
             error_msg =form.errors
             return render(request,"wly_app/body.html",{'form':form,"errors":error_msg})
