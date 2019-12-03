807e16ee0c8b5dde8e93784721c7d13593f5f434
icccle <clee218@163.com>
2019-12-03 16:59:05 +0800

    last time

--- ly_app/views.py
+++ ly_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
 def ly_test(request):
-	return render(request,'ly_app/test.html')
+	# 获取用户数据
+	course_list = models.course.objects.all()
+
+	return render(request,'ly_app/test.html',locals())
