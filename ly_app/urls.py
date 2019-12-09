a75203f7ed584a0bfbf9fb51b5c302fe03dfd607
icccle <clee218@163.com>
2019-12-10 00:56:49 +0800

    增加info

--- ly_app/urls.py
+++ ly_app/urls.py
@@ -5,5 +5,6 @@ app_name = 'ly_app'
 
 urlpatterns = [
     path('course/',views.ly_test,name='ly_test'),
-    path('coach/',views.ly_coach,name="ly_coach")
+    path('coach/',views.ly_coach,name="ly_coach"),
+    path('info/',views.ly_info,name="ly_info")
 ]
--- ly_app/views.py
+++ ly_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . import models
+from users import models
 # Create your views here.
 def ly_test(request):
 	# 获取用户数据
@@ -8,3 +9,7 @@ def ly_test(request):
 	return render(request,'ly_app/course.html',locals())
 def ly_coach(request):
 	return render(request,'ly_app/coach.html')
+def ly_info(request):
+	username = request.session.get('user_name')
+	info_list = models.New_User.objects.get(login_name = username)
+	return render(request,'ly_app/info.html',locals())
